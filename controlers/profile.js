47ff37bb157f3f8cdc1e5fb6688467eebf218cc8
arnabadhikaryy <139059216+arnabadhikaryy@users.noreply.github.com>
2025-06-05 21:04:10 +0530

    Update profile.js

--- controlers/profile.js
+++ controlers/profile.js
@@ -1,7 +1,29 @@
-//import authenticateJWT from "../middleware/authinticateToken.js";
+//edited by: [arnab]
 
-function profile(req,res){
-    res.send({status:true,message:req.JsonUserInfo})
+import User from "../Schema/userSchema.js";
+
+async function profile(req,res){
+    console.log('middelware details: ', req.JsonUserInfo)
+
+    try {
+        // Find user by ID from the token
+        let response = await User.findOne({ _id: req.JsonUserInfo.userId });
+        if (response) {
+            // Send user data in response
+            return res.send({
+                status: true,
+                message: 'User profile retrieved successfully',
+                data: response
+            });
+        } else {
+            return res.send({ status: false, message: 'User not found' });
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return res.send({ status: false, message: 'Oops, something went wrong!' });
+    }
+    //res.send({status:true,message:req.JsonUserInfo})
 }
 
 export {profile};
